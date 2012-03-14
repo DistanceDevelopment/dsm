b23f2397aaaf07033052de25b0e0006253557e53
David Lawrence Miller <dave@ninepointeightone.net>
2012-03-14 15:26:19 -0400

    started documenting mbb

--- R/dsm.fit.r
+++ R/dsm.fit.r
@@ -89,243 +89,261 @@ dsm.fit <- function(ddfobject, phat=NULL, response, formula,
 #   eval(parse(text=field.names))
    # above code just does this, but it is a bit obtuse
    # probably want to do something smart here...
-   y.name<-'y'
-   x.name<-'x'
-   seglength.name<-'Effort'
-   segnum.name<-'Sample.Label'
-   distance.name<-'distance'
-   cluster.name<-'size'
-   esw.name<-'esw'
-   sightnum.name<-'object'
-   sampling.fraction.name<-'SF'
+  y.name<-'y'
+  x.name<-'x'
+  seglength.name<-'Effort'
+  segnum.name<-'Sample.Label'
+  distance.name<-'distance'
+  cluster.name<-'size'
+  esw.name<-'esw'
+  sightnum.name<-'object'
+  sampling.fraction.name<-'SF'
 
-   # Truncate observations made at distances greater than the truncation width;
-   # truncation value picked up from metadata contained within ddfobject
-   # No truncation for strip transects
-   if (!is.null(ddfobject)){
-      obsdata<-obsdata[obsdata[,distance.name]<=ddfobject$meta.data$width,]
-   }
+  # Truncate observations made at distances greater than the truncation width;
+  # truncation value picked up from metadata contained within ddfobject
+  # No truncation for strip transects
+  if (!is.null(ddfobject)){
+     obsdata<-obsdata[obsdata[,distance.name]<=ddfobject$meta.data$width,]
+  }
 
-   #  the following is borrowed (heavily) from dsm.count by Laake
-   #  ER modification is to test for presence of phat argument and substitute 
-   #     detection probabilities from phat if provided
-   if(response=="indiv.est" | response=="group.est" | 
-         response=="indiv.den" | response=="group.den"){
-      if(!is.null(phat)){
-         fitted.p<-phat
-         object.data<-obsdata$sightnum.name
-      }else{
-         fitted.p<-ddfobject$fitted
-         object.data<-names(ddfobject$fitted)
-      }
-      sig.prob<-data.frame(p=fitted.p, object=object.data)
-      # old stmt: sig.prob <- data.frame(p = ddfobject$fitted, 
-      #                                  object = names(ddfobject$fitted))
+  #  the following is borrowed (heavily) from dsm.count by Laake
+  #  ER modification is to test for presence of phat argument and substitute 
+  #     detection probabilities from phat if provided
+  if(response=="indiv.est" | response=="group.est" | 
+       response=="indiv.den" | response=="group.den"){
+    if(!is.null(phat)){
+      fitted.p<-phat
+      object.data<-obsdata$sightnum.name
+    }else{
+      fitted.p<-ddfobject$fitted
+      object.data<-names(ddfobject$fitted)
+    }
+    sig.prob<-data.frame(p=fitted.p, object=object.data)
+    # old stmt: sig.prob <- data.frame(p = ddfobject$fitted, 
+    #                                  object = names(ddfobject$fitted))
 
-      # Merge observations with sighting probabilities
-      obsdata <- merge(obsdata, sig.prob, by=sightnum.name, all.x=T, sort=F)
-      # Revision 10 Nov 2008; merge drops segments when detects are 
-      # not made by primary see MLB and CODA
-      obsdata <- obsdata[!is.na(obsdata$p), ]       
+    # Merge observations with sighting probabilities
+    obsdata <- merge(obsdata, sig.prob, by=sightnum.name, all.x=T, sort=F)
+    # Revision 10 Nov 2008; merge drops segments when detects are 
+    # not made by primary see MLB and CODA
+    obsdata <- obsdata[!is.na(obsdata$p), ]       
 
-      # Check to see if any of the observations are missing a detection 
-      # probability for designs of type 'trial' objects observed by 
-      # trackers will not have computed detection probabilities, so, trap 
-      # that type of design, interrogating the call to ddf (archived in 
-      # ddfobject$call using an archane function 'languageEl'
-      field.design<-substr(languageEl(ddfobject$call, which="method"),1,5)
+    # Check to see if any of the observations are missing a detection 
+    # probability for designs of type 'trial' objects observed by 
+    # trackers will not have computed detection probabilities, so, trap 
+    # that type of design, interrogating the call to ddf (archived in 
+    # ddfobject$call using an archane function 'languageEl'
+    field.design<-substr(languageEl(ddfobject$call, which="method"),1,5)
 
-      if(field.design!="trial" && any(is.na(obsdata$p))){
-         cat("The following sighting numbers don't have a matching detection probability\n")
-         print(obsdata[,sightnum.name][is.na(obsdata$p)])
-         stop("Function terminated")
-      }
-   }
+    if(field.design!="trial" && any(is.na(obsdata$p))){
+      cat("The following sighting numbers don't have a matching detection probability\n")
+      print(obsdata[,sightnum.name][is.na(obsdata$p)])
+      stop("Function terminated")
+    }
+  }
 
-   # If response is group or group.est - then change all cluster values to 1
-   # if density is the responnse, then the response variable needs to 
-   # be # detected divided by area!!!
-   if(response=="group" | response=="group.est"){
-       obsdata[,cluster.name]<-rep(1,dim(obsdata)[1])
-   }
+  # If response is group or group.est - then change all cluster values to 1
+  # if density is the responnse, then the response variable needs to 
+  # be # detected divided by area!!!
+  if(response=="group" | response=="group.est"){
+    obsdata[,cluster.name]<-rep(1,dim(obsdata)[1])
+  }
 
-   # Aggregate response values of the sightings over segments
-   if(response=="indiv" | response=="group"){
-      responsedata<-aggregate(obsdata[,cluster.name],
-                       list(obsdata[,segnum.name]), sum)
-       off.set <- "eff.area"
-   }else if (response=="indiv.est" | response=="group.est"){
-      responsedata<-aggregate(obsdata[,cluster.name]/obsdata$p,
-                              list(obsdata[,segnum.name]), sum)
-      off.set<-"area"
-   }else{
-      responsedata <- aggregate(obsdata[,cluster.name]/obsdata$p,
-                                list(obsdata[,segnum.name]), sum) 
-      off.set <- "none"
-   }
-   
-   names(responsedata)<-c(segnum.name,"N")
+  # Aggregate response values of the sightings over segments
+  if(response=="indiv" | response=="group"){
+    responsedata<-aggregate(obsdata[,cluster.name],
+                            list(obsdata[,segnum.name]), sum)
+    off.set <- "eff.area"
 
-   # Next merge the response variable with the segment records and any 
-   # response variable that is NA should be assigned 0 because these 
-   # occur due to 0 sightings
-   dat<-merge(segdata,responsedata,by=segnum.name,all.x=T)
-   dat$N[is.na(dat$N)]<-0
-   # With density, we need to transform response variable to a density 
-   # by dividing by area    
-   if (off.set=="none"){
-      dat$N<-dat$N/2*dat[,seglength.name]*ddfobject$meta.data$width*convert.units
-   }
-   # when density is response, offset should be 1.
-   dat$off.set<-switch(off.set,
-                       eff.area=2*dat[,seglength.name]*dat[,esw.name],
-                       area=2*dat[,seglength.name]*ddfobject$meta.data$width,
-                       none=1)
-   # Altered 2 Feb 06 to use final argument passed into function 
-   # from InputFileMaker
-   if(!is.null(convert.units) & off.set!="none"){
-      dat$off.set<-dat$off.set*convert.units
-   }
+    # need to find the fitted ps to divide through by below
+    # we don't have individual level covariates, so there is only
+    # one p
+  
+    # did the user supply phat?
+    if(!is.null(phat)){
+      fitted.p<-unique(phat)
+    }else if(any(names(obsdata)==esw.name)){
+      fitted.p<-unique(obsdata[,esw.data]*ddfobject$width)
+    }else{
+      fitted.p<-unique(ddfobject$fitted)
+    }    
 
-   # Set offset as log of area or effective area
-   if(off.set!="none"){
-      dat$off.set<-log(dat$off.set)
-   }
+    if(length(fitted.p)!=1){
+      stop(paste("More than one unique probability of detection.\n",
+                 "Are you sure this is an \"indiv\"/\"group\" analysis?"))
+    }
+  }else if (response=="indiv.est" | response=="group.est"){
+    responsedata<-aggregate(obsdata[,cluster.name]/obsdata$p,
+                            list(obsdata[,segnum.name]), sum)
+    off.set<-"area"
+  }else{
+    responsedata <- aggregate(obsdata[,cluster.name]/obsdata$p,
+                              list(obsdata[,segnum.name]), sum) 
+    off.set <- "none"
+  }
+  
+  # we'll just call the response N, whatever we're actually modelling
+  names(responsedata)<-c(segnum.name,"N")
 
-   # Create formula 
-   if(response=="indiv.den" | response=="group.den"){
-      formula<-as.formula(paste("N", deparse(formula,width.cutoff=500),
-                                  collapse=""))
-   }else{
-      formula<-as.formula(paste("N", deparse(formula,width.cutoff=500),
-                                  "+ offset(off.set)",collapse=""))
-   }
+  # Next merge the response variable with the segment records and any 
+  # response variable that is NA should be assigned 0 because these 
+  # occur due to 0 sightings
+  dat<-merge(segdata,responsedata,by=segnum.name,all.x=T)
+  dat$N[is.na(dat$N)]<-0
+  # With density, we need to transform response variable to a density 
+  # by dividing by area    
+  if (off.set=="none"){
+    dat$N<-dat$N/2*dat[,seglength.name]*ddfobject$meta.data$width*convert.units
+  }
 
-   ###########################################
-   ### Response distribution, link function etc
-   # Paste link function argument together with family argument to 
-   # present to gam/glm in the form:  family=quasipoisson(link="log")
-   if(model.defn$family=="Tweedie"){
-      # need to specify the Tweedie parameters
-      if(is.null(model.defn$family.pars$p)){
-         error("You must specify the p parameter to use the Tweedie family! See ?Tweedie.")
-      }
-      family.and.link<-eval(parse(text=paste(model.defn$family,
-                          "(link='", link, "',p=",model.defn$family.pars$p,")",
-                                             sep="")))
-   }else if(model.defn$family=="quasi"){
-      # specify the variance relationship for quasi
-      if(is.null(model.defn$family.pars$variance)){
-         error("You must specify the variance relation to use the quasi family! See ?family.")
-      }
-      family.and.link<-eval(parse(text=paste(model.defn$family,
-                             "(link='", link, 
-                          "',variance='",model.defn$family.pars$variance,"')",
-                                             sep="")))
-   }else{
-      # if the family does not have extra parameters
-      family.and.link<-eval(parse(text=paste(model.defn$family, 
-                            "(link='", link, "')", sep="")))
-   }
+  # when density is response, offset should be 1.
+  dat$off.set<-switch(off.set,
+                      eff.area=2*dat[,seglength.name]*fitted.p,
+                      area=2*dat[,seglength.name]*ddfobject$meta.data$width,
+                      none=1)
+  # Altered 2 Feb 06 to use final argument passed into function 
+  # from InputFileMaker
+  if(!is.null(convert.units) & off.set!="none"){
+    dat$off.set<-dat$off.set*convert.units
+  }
 
-   if (!is.null(wghts)){
-      wghts<-paste("dat$", wghts, sep="")
-   }
-   dat$area<-2*dat[,seglength.name]*ddfobject$meta.data$width*convert.units
+  # Set offset as log of area or effective area
+  if(off.set!="none"){
+    dat$off.set<-log(dat$off.set)
+  }
 
-   if(toupper(model.defn$fn)=="GAM"){
+  # Create formula 
+  if(response=="indiv.den" | response=="group.den"){
+    formula<-as.formula(paste("N", deparse(formula,width.cutoff=500),
+                              collapse=""))
+  }else{
+    formula<-as.formula(paste("N", deparse(formula,width.cutoff=500),
+                                "+ offset(off.set)",collapse=""))
+  }
 
-      # if we are doing soap film smoothing, we need make sure that we
-      # don't mess up the knots
-      if(grepl('bs = "so"',as.character(formula)[3])){
+  ###########################################
+  ### Response distribution, link function etc
+  # Paste link function argument together with family argument to 
+  # present to gam/glm in the form:  family=quasipoisson(link="log")
+  if(model.defn$family=="Tweedie"){
+    # need to specify the Tweedie parameters
+    if(is.null(model.defn$family.pars$p)){
+       error("You must specify the p parameter to use the Tweedie family! See ?Tweedie.")
+    }
+    family.and.link<-eval(parse(text=paste(model.defn$family,
+                         "(link='", link, "',p=",model.defn$family.pars$p,")",
+                                           sep="")))
+  }else if(model.defn$family=="quasi"){
+    # specify the variance relationship for quasi
+    if(is.null(model.defn$family.pars$variance)){
+       error("You must specify the variance relation to use the quasi family! See ?family.")
+    }
+    family.and.link<-eval(parse(text=paste(model.defn$family,
+                           "(link='", link, 
+                        "',variance='",model.defn$family.pars$variance,"')",
+                                           sep="")))
+  }else{
+    # if the family does not have extra parameters
+    family.and.link<-eval(parse(text=paste(model.defn$family, 
+                          "(link='", link, "')", sep="")))
+  }
 
-         # the boundary must be name bnd in the formula, 
-         # otherwise this breaks
-         bnd<-model.defn$bnd
-         knots<-model.defn$knots
-   
-         if(is.null(wghts)){
-            b<-try(gam(formula,family=family.and.link,data=dat,
-                   control=gam.control(keepData=TRUE),
-                   weights=NULL, gamma=1.4,...))
-         }else{
-            b<-try(gam(formula, family=family.and.link, data=dat,
-                   control=gam.control(keepData=TRUE),
-                   weights=eval(parse(text=wghts)),gamma=1.4,...))
-         }
+  if (!is.null(wghts)){
+    wghts<-paste("dat$", wghts, sep="")
+  }
+  dat$area<-2*dat[,seglength.name]*ddfobject$meta.data$width*convert.units
 
-         # loop until the knots are okay but not more than 5 times
-         max.wiggles<-1
-         while((b[[1]]==
-            "Error in check.knots(g) : Please (re)move problematic knots.\n") & 
-            (max.wiggles<6)
-              ){
+  if(toupper(model.defn$fn)=="GAM"){
+    # if we are doing soap film smoothing, we need make sure that we
+    # don't mess up the knots
+    if(grepl('bs = "so"',as.character(formula)[3])){
 
-            # find the problem knots
-            warning.mess<-names(last.warning)
-            problem.knots<-as.numeric(
-                      gsub("knot ([0-9]+) is in boundary cell of solution grid",
-                                   "\\1",warning.mess))
-
-            # wiggle them
-            for(i in problem.knots){
-               this.knot<-knots[i,]
-               this.knot<-this.knot+runif(2,-runif.scale,runif.scale)
-               while(!inSide(bnd,this.knot[1],this.knot[2])){
-                  this.knot<-knots[i,]
-                  this.knot<-this.knot+runif(2,-runif.scale,runif.scale)
-               }
-               knots[i,]<-this.knot
-            }
-
-            # refit the model
-            if(is.null(wghts)){
-               b<-try(gam(formula,family=family.and.link,data=dat,
-                      control=gam.control(keepData=TRUE),
-                      weights=NULL, gamma=1.4,...))
-            }else{
-               b<-try(gam(formula, family=family.and.link, data=dat,
-                      control=gam.control(keepData=TRUE),
-                      weights=eval(parse(text=wghts)),gamma=1.4,...))
-            }
-
-            max.wiggles<-max.wiggles+1
-         }
-
-         # if we had too many wiggles above
-         if(max.wiggles==5){
-            stop("Too many soap knot failures! Try different knots!")
-         }
-
-      }else{
-         # Non-soap model
-         # Fit model  hardwiring gamma=1.4 per Wood (2006:254) who cites 
-         # Kim and Gu(2004) for overfitting weights should be 'area' when 
-         # density is response.
-         if(is.null(wghts)){
-            b<-gam(formula,family=family.and.link,data=dat,
-                   control=gam.control(keepData=TRUE),weights=NULL, gamma=1.4)
-         }else{
-            b<-gam(formula, family=family.and.link, data=dat,
-                   control=gam.control(keepData=TRUE),
-                   weights=eval(parse(text=wghts)),gamma=1.4)
-         }
-      }
-
-
-   }else{
-      # GLM case
+      # the boundary must be name bnd in the formula, 
+      # otherwise this breaks
+      bnd<-model.defn$bnd
+      knots<-model.defn$knots
+  
       if(is.null(wghts)){
-         b<-glm(formula,family=family.and.link,data=dat,control=glm.control(),
-                                    weights=NULL,gamma=1.4)
+        b<-try(gam(formula,family=family.and.link,data=dat,
+               control=gam.control(keepData=TRUE),
+               weights=NULL, gamma=1.4,...))
       }else{
-         b<-glm(formula,family=family.and.link,data=dat,control=glm.control(),
-                                  weights=eval(parse(text=wghts)),gamma=1.4)
+        b<-try(gam(formula, family=family.and.link, data=dat,
+               control=gam.control(keepData=TRUE),
+               weights=eval(parse(text=wghts)),gamma=1.4,...))
       }
-   }
-   # Return model object
-   ret<-list(result=b,call.dsm=match.call(),data=dat)
-   class(ret)<-"dsm"
 
-   return(ret)
+      # loop until the knots are okay but not more than 5 times
+      max.wiggles<-1
+      while((b[[1]]==
+        "Error in check.knots(g) : Please (re)move problematic knots.\n") & 
+        (max.wiggles<6)){
+
+        # find the problem knots
+        warning.mess<-names(last.warning)
+        problem.knots<-as.numeric(
+                  gsub("knot ([0-9]+) is in boundary cell of solution grid",
+                               "\\1",warning.mess))
+
+        # wiggle them
+        for(i in problem.knots){
+           this.knot<-knots[i,]
+           this.knot<-this.knot+runif(2,-runif.scale,runif.scale)
+           while(!inSide(bnd,this.knot[1],this.knot[2])){
+              this.knot<-knots[i,]
+              this.knot<-this.knot+runif(2,-runif.scale,runif.scale)
+           }
+           knots[i,]<-this.knot
+        }
+
+        # refit the model
+        if(is.null(wghts)){
+           b<-try(gam(formula,family=family.and.link,data=dat,
+                  control=gam.control(keepData=TRUE),
+                  weights=NULL, gamma=1.4,...))
+        }else{
+           b<-try(gam(formula, family=family.and.link, data=dat,
+                  control=gam.control(keepData=TRUE),
+                  weights=eval(parse(text=wghts)),gamma=1.4,...))
+        }
+
+         max.wiggles<-max.wiggles+1
+      }
+
+      # if we had too many wiggles above
+      if(max.wiggles==5){
+        stop("Too many soap knot failures! Try different knots!")
+      }
+
+    }else{
+       # Non-soap model
+       # Fit model  hardwiring gamma=1.4 per Wood (2006:254) who cites 
+       # Kim and Gu(2004) for overfitting weights should be 'area' when 
+       # density is response.
+       if(is.null(wghts)){
+         b<-gam(formula,family=family.and.link,data=dat,
+                control=gam.control(keepData=TRUE),weights=NULL, gamma=1.4)
+       }else{
+         b<-gam(formula, family=family.and.link, data=dat,
+                control=gam.control(keepData=TRUE),
+                weights=eval(parse(text=wghts)),gamma=1.4)
+       }
+     }
+
+
+  }else{
+    # GLM case
+    if(is.null(wghts)){
+       b<-glm(formula,family=family.and.link,data=dat,control=glm.control(),
+                                  weights=NULL,gamma=1.4)
+    }else{
+       b<-glm(formula,family=family.and.link,data=dat,control=glm.control(),
+                                weights=eval(parse(text=wghts)),gamma=1.4)
+    }
+  }
+  # Return model object
+  ret<-list(result=b,call.dsm=match.call(),data=dat)
+  class(ret)<-"dsm"
+
+  return(ret)
 }
--- R/param.movblk.variance.r
+++ R/param.movblk.variance.r
@@ -7,22 +7,23 @@
 # documentation
 # add in detection function uncertainty stuff
 
-# parametric moving block bootstrap to obtain variance
-param.movblk.variance <- function(B, dsm.object, pred.object, 
-                                  samp.unit.name='Transect',block, 
-                                  field=FALSE, cell.name, bpfile){
+#' Variance estimation via parametric moving block bootstrap
+#'
 
-# B=num.boot -- number of resamples
-# dsm.object=dsm.object -- object returned from dsm.fit()
-# pred.object=predict.object -- this used to be more complicated, for now
-#                          just a data frame as would usually get passed to 
-#                          predict.gam() 
-# samp.unit.name='Transect' -- name of the (re)sampling unit
-# block=num.segs.in.block
-# field=FALSE [[this and cell.name are linked, see below]]
-# cell.name=cell [[size of the cell to convert density to abundance]]
-# bpfile -- file used by distance to get the progress bar -- REMOVE?
-#           ignored at the moment
+param.movblk.variance <- function(n.boot, dsm.object, pred.object, 
+                                  samp.unit.name='Transect',block.size, 
+                                  field=FALSE, cell.name, bpfile=NULL){
+
+#' @param n.boot number of bootstrap resamples.
+#' @param dsm.object object returned from \code{\link{dsm.fit()}}.
+#' @param pred.grid a \code{data.frame} that holds prediction points, must have
+#'        the correct columns. 
+#' @param samp.unit.name name sampling unit to resample (default 'Transect').
+#' @param block.size number of segments in each block.
+#' @param field=FALSE [[this and cell.name are linked, see below]]
+#' @param cell.name=cell [[size of the cell to convert density to abundance]]
+#' @param bpfile path to a file to be used (usually by Distance) to get 
+#'        generate a progress bar (default \code{NULL} -- not used).
 
   #prediction.call <- pred.object$call
   # previously, prediction.call[3] held the name for the prediction data
@@ -69,23 +70,25 @@ param.movblk.variance <- function(B, dsm.object, pred.object,
                resids.when.missing(model=dsm.object$result)
 
   # Sort out blocks for each sampling unit
-  block.info <- block.info.per.su.f(block=block,
-                                    data=dsm.object$result$data,
-                                    name.su=name.sampling.unit)
+  block.info <- block.info.per.su(block.size=block.size,
+                                  data=dsm.object$result$data,
+                                  name.su=name.sampling.unit)
   tot.num.blocks <- sum(block.info$num.block)
   k <- sum(block.info$num.req)
   block.vector <- 1:tot.num.blocks
 
   # Start bootstrapping
-  for(i in 1:B){
+  for(i in 1:n.boot){
     # Compute proportion of bootstrapping completed, write it to file 
     # passed as argument 'bpfile'
     # This file will be read by Distance to present a progress bar.
-    #progress <- round(i/B, 2)* 100
-    #write(progress, file=bpfile, append=FALSE)
+    if(!is.null(bpfile)){
+      progress <- round(i/n.boot, 2)* 100
+      write(progress, file=bpfile, append=FALSE)
+    }
 
     bs.blocks <- sample(block.vector, k, replace=TRUE)
-    bs.resids <- generate.mb.sample.f(k = k, len = block, 
+    bs.resids <- generate.mb.sample(k = k, block.size = block.size, 
                        which.blocks = bs.blocks, x = dsm.object$result$data, 
                        unit.info = block.info, n.units = num.sampling.unit)
 
@@ -126,16 +129,16 @@ param.movblk.variance <- function(B, dsm.object, pred.object,
 }
 
 
-# sort out the block information for each sampling unit
-block.info.per.su.f <- function(block,data,name.su){
-# block=block.size,data=dsm.data,name.su=unique.sampling.units
+# sort out the block information 
+block.info.per.su <- function(block.size,data,name.su){
+# block.size=block.size,data=dsm.data,name.su=unique.sampling.units
   unit <- NULL   
   unit$name <- name.su
   num.su <- length(name.su)
   for (i in 1:num.su) {
     unit$num[i] <- length(
                      data$sampling.unit[data$sampling.unit == unit$name[i]])
-    unit$num.block[i] <- unit$num[i] - block + 1
+    unit$num.block[i] <- unit$num[i] - block.size + 1
     if(i == 1){
       unit$start.block[i] <- 1
     }else{
@@ -145,7 +148,7 @@ block.info.per.su.f <- function(block,data,name.su){
   unit$end.block <- cumsum(unit$num.block)
   # Get number of blocks required for each sampling unit  -- 
   #  samp units different sizes so need to get more observations than required 
-  unit$num.req <- ceiling(unit$num/block)
+  unit$num.req <- ceiling(unit$num/block.size)
   unit$num.req[unit$num.req==0] <- 1
   unit <- data.frame(unit)
 
@@ -154,9 +157,10 @@ block.info.per.su.f <- function(block,data,name.su){
 
 
 # generates the vector of residuals which can be mapped back onto the data
-generate.mb.sample.f <- function(k, len, which.blocks, x, unit.info, n.units){
+generate.mb.sample <- function(k, block.size, which.blocks, x, 
+                               unit.info, n.units){
 # k = required.num.blocks
-# len = len.block
+# block.len = len.block
 # which.blocks = bs.blocks
 # x = data
 # unit.info = unit.info
@@ -180,7 +184,7 @@ generate.mb.sample.f <- function(k, len, which.blocks, x, unit.info, n.units){
     x.unit <- x[x$sampling.unit == bs$unit.name[i], ]            
     x.unit <- data.frame(x.unit)
     start.row <- bs$unit.block[i]
-    end.row <- start.row + len - 1
+    end.row <- start.row + block.size - 1
     x.block <- x.unit[start.row:end.row, ]
     x.block <- data.frame(x.block)
     if (i == 1){
@@ -195,7 +199,7 @@ generate.mb.sample.f <- function(k, len, which.blocks, x, unit.info, n.units){
   temp <- bs.data$log.resids
   for (j in 1:n.units) {
     # Get number of observations in blocks
-    tb <- unit.info$num.req[j] * len  
+    tb <- unit.info$num.req[j] * block.size
     tran.resids <- temp[1:unit.info$num[j]]
     temp <- temp[-(1:tb)] 
     if (j==1){
