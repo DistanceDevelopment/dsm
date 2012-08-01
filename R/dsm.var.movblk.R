af052bea62f9f3104fb21260eb0ff6fd44d7f324
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-01 12:56:52 -0400

    improvements to bootstrap code

--- R/dsm.var.movblk.R
+++ R/dsm.var.movblk.R
@@ -12,10 +12,9 @@
 #' @param n.boot number of bootstrap resamples.
 #' @param block.size number of segments in each block.
 #' @param off.set offset to be used in the model, see \code{\link{offsets}}.
-#'        Note that this is NOT logged.
-#' @param ds.uncertainty incorporate uncertainty in the detection function?
-#'        Note that this doesn't work for covariate models for the detection
-#'        function at the moment.
+#'        Note that this should NOT be \code{log()}'d.
+#' @param ds.uncertainty incorporate uncertainty in the detection function? See
+#'        Details, below. Note that this feature is EXPERIMENTAL at the moment.
 #' @param samp.unit.name name sampling unit to resample (default 
 #'        'Transect.Label').
 #' @param progress.file path to a file to be used (usually by Distance) to 
@@ -24,10 +23,23 @@
 #'        of the bootstrap results rather than just the summaries, enabling
 #'        outliers to be detected and removed. (Default \code{NULL}).
 #' @param bar should a progress bar be printed to screen? (Default \code{TRUE}).
+#'
+#' @section Details: 
+#'  Setting \code{ds.uncertainty=TRUE} will incorporate detection function 
+#'  uncertainty directly into the bootstrap. This is done by generating 
+#'  observations from the fitted detection function and then re-fitting a new 
+#'  detection function (of the same form), then calculating a new effective 
+#'  strip width. Rejection sampling is used to generate the observations (except
+#'  in the half-normal case) so the procedure can be rather slow. Note that this
+#'  is currently not supported with covariates in the detection function.
+#'
+#'  Setting \code{ds.uncertainty=FALSE} will incorporate detection function 
+#'  uncertainty using the delta method. This assumes that the detection function
+#'  and the spatial model are INDEPENDENT. This is probably not reasonable.
+#'
 #' @export
 
 ## TODO
-# documentation
 # make up the missed replicates due to model errors?
 # detection function uncertainty:
 #  * new sampler doesn't do point transects at the moment
@@ -99,10 +111,54 @@ dsm.var.movblk <- function(dsm.object, pred.data, n.boot, block.size,
     pb <- txtProgressBar(min=0,max=n.boot,style=3)
   }
 
+  # first resample is the model predicitons themselves
+
+  # if we supplied bs.file, then write detailed, per replicate
+  # if it's the first time and the file already exists, modify the name 
+  # use that
+  if(!is.null(bs.file)){
+    if(file.exists(bs.file)){
+      a.number <- 1
+      bs.file2 <- strsplit(bs.file,"\\.")[[1]]
+      bs.file2.end <- paste('.',bs.file2[length(bs.file2)],sep="")
+      bs.file2.start <- paste(bs.file2[1:(length(bs.file2)-1)],collapse=".")
+      bs.file2 <- paste(bs.file2.start,"-",a.number,bs.file2.end,
+                        collapse="",sep="")
+      while(file.exists(bs.file2)){
+        a.number <- a.number+1
+        bs.file2 <- paste(bs.file2.start,"-",a.number,bs.file2.end,
+                          collapse="",sep="")
+      }
+      warning(paste("Filename",bs.file,"was taken, writing to file",
+                     bs.file2))
+      bs.file<-bs.file2
+    }
+  }
+
+  dsm.predict.bs <- try(dsm.predict(dsm.object,
+                                    newdata=pred.data,
+                                    off=off.set))
+  if(all(class(dsm.predict.bs)=="try-error")){
+    dsm.predict.bs <- rep(NA,length(short.var$sumx))
+  }
+
+  # Don't save all cell values for all reps, rather, 
+  #  populate dataframe with machine formula components for each cell
+  vector.cell.abundances <- dsm.predict.bs 
+  short.var$sumx <- short.var$sumx + vector.cell.abundances
+  short.var$sumx.sq <- short.var$sumx.sq + vector.cell.abundances^2
+  study.area.total[1] <- sum(vector.cell.abundances, na.rm=TRUE)
+
+  if(!is.null(bs.file)){
+    # append to the file bs.file
+    write.table(t(dsm.predict.bs),bs.file,append=TRUE,
+                sep=",",col.names=FALSE)
+  }
+
   off.set.save<-off.set
 
   # Start bootstrapping
-  for(i in 1:n.boot){
+  for(i in 2:n.boot){
     # Compute proportion of bootstrapping completed, write it to file 
     # passed as argument 'progress.file'
     # This file will be read by Distance to present a progress bar.
@@ -200,34 +256,11 @@ dsm.var.movblk <- function(dsm.object, pred.data, n.boot, block.size,
       short.var$sumx.sq <- short.var$sumx.sq + vector.cell.abundances^2
       study.area.total[i] <- sum(vector.cell.abundances, na.rm=TRUE)
 
-      # if we supplied bs.file, then write detailed, per replicate
-      # data
-      # if it's the first time and the file already exists, modify the name 
-      # use that
-      if(!is.null(bs.file) & i==1){
-        if(file.exists(bs.file)){
-          a.number <- 1
-          bs.file2 <- strsplit(bs.file,"\\.")[[1]]
-          bs.file2.end <- paste('.',bs.file2[length(bs.file2)],sep="")
-          bs.file2.start <- paste(bs.file2[1:(length(bs.file2)-1)],collapse=".")
-          bs.file2 <- paste(bs.file2.start,"-",a.number,bs.file2.end,
-                            collapse="",sep="")
-          while(file.exists(bs.file2)){
-            a.number <- a.number+1
-            bs.file2 <- paste(bs.file2.start,"-",a.number,bs.file2.end,
-                              collapse="",sep="")
-          }
-          warning(paste("Filename",bs.file,"was taken, writing to file",
-                         bs.file2))
-          bs.file<-bs.file2
-        }
-      }
       if(!is.null(bs.file)){
         # append to the file bs.file
         write.table(t(dsm.predict.bs),bs.file,append=TRUE,
                     sep=",",col.names=FALSE)
       }
-
     }else{
       study.area.total[i] <- NA
     }
@@ -246,14 +279,11 @@ dsm.var.movblk <- function(dsm.object, pred.data, n.boot, block.size,
                  n.boot=n.boot, 
                  off.set=original.offset,
                  block.size=block.size,
-                 bs.file=bs.file
+                 bs.file=bs.file,
+                 dsm.object=dsm.object
                 )
 
-  # package up the ddf result if we did detection function uncertainty
-#  if(!ds.uncertainty){
-    result$dsm.object <- dsm.object
-#  }
-
+  # give it a bit of class
   class(result)<-c("dsm.var")
 
   if(bar){
