04f677df6d8cd114f2387f007e11c3cf61bc66f3
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-20 19:48:19 -0400

    cleanup of dsm.predict

--- R/dsm.predict.R
+++ R/dsm.predict.R
@@ -10,25 +10,10 @@
 #'                sea temperature, depth, distance to shore, etc.
 #'                Note covariates in this dataframe must have names *identical* 
 #'                to variable names used in fitting the GAM.
-#   Known as 'prediction.covariates.dat.r' in D6 DSM statement of 
-#   direction prediction grid data frame.
-#'                
 #' @param off.set offset for the cells in the prediction grid (\code{newdata}). 
-#'                NB this should NOT be logged, it gets logged below.
-#     Assume if a constant comes in, it will already have had the link
-#     transform applied, whereas if field, apply link fn.
-#### Commented out for the moment, not sure how useful it is...
-# @param silent Set to true when used in conjunction with variance bootstraps; in 
-#               this case we don't wish to see the abundance estimate printed
-#'
+#'                NB this should NOT have the link function applied already, 
+#'                this happens below. (For example, it should _not_ be logged).
 #' @return array of predicted values.
-
-#### Removed this too, not sure why we need the call here...
-# @return predicted list consisting of:
-#          \tabular{ll}{\code{result} \tab one-dimensional array of predicted 
-#                       values (without se's)\cr
-#                       \code{call} \tab the call that generated this instance 
-#                       of the function}
 
 #' @note Presently, only density surface models fitted with \code{\link{mgcv}} 
 #'       can be used for prediction with this incarnation of \code{dsm.predict}.
@@ -38,12 +23,8 @@
 #'
 #' Wood, S.N. 2006. Generalized Additive Models: An Introduction with R. CRC/Chapman & Hall.
 #' @export
-#   Functions used:  predict.gam() from package mgcv
-
-dsm.predict<-function(model, newdata=NULL, off.set=NULL){#, 
-#                      silent=FALSE){
-
-  off <- off.set
+# Functions used:  predict.gam() from package mgcv
+dsm.predict<-function(model, newdata=NULL, off.set=NULL){ 
 
   # do we have a dsm object or a gam?
   if("dsm" %in% class(model)){
@@ -57,35 +38,20 @@ dsm.predict<-function(model, newdata=NULL, off.set=NULL){#,
   if(is.null(newdata)){
     prediction.grid<-model$data
   }else{
-
-
-    #  Append cell size of prediction grid to prediction grid  if off.set 
-    #  argument is a number, otherwise manufacture
-    if(length(off)==1){
-      off<-rep(off,nrow(newdata))
+    # if we only got supplied 1 offset value, duplicate it enough times
+    if(length(off.set)==1){
+      off.set <- rep(off.set,nrow(newdata))
     }
 
-    off<-log(off)
+    # apply the link function
+    linkfn <- gam.model$family$linkfun
+    off <- linkfn(off)
 
+    # Append cell size of prediction grid to prediction grid  
     prediction.grid <- data.frame(newdata, off.set=off)
-    #else{
-    #  # match.call()[] provides literal strings of the arguments passed to this 
-    #  # function; I wish to build a string consisting of the string associated with 
-    #  # the 3rd argument concatenated with the string of the 5th argument; in 
-    #  # combination they give the name of the field in the dataframe containing 
-    #  # cell size
-    #  # if(gam.model$result$family$link=="log"){ # suggested by Louise 22 Sept 2008
-    #  if(gam.model$family$link=="log"){
-	  #    newdata$off.set<-eval(parse(text=paste("log(", match.call()[3], 
-    #                           "$", match.call()[5], ")", sep=""))) 
-    #  }else{
-    #     newdata$off.set<-eval(parse(text=paste(match.call()[3], 
-    #                           "$", match.call()[5], sep="")))
-    #  }
-    #  prediction.grid <- newdata 
-    #}
   }
 
+  # actually do the predict call
   result<-predict(gam.model, newdata=prediction.grid, 
                   type="response", na.action=na.pass)
 
