9a9687825da87d0b83787408207db695a5169ceb
David Lawrence Miller <dave@ninepointeightone.net>
2012-09-19 14:47:02 -0400

    and by las=2, I of course meant las=1

--- R/dsm.check.R
+++ R/dsm.check.R
@@ -67,7 +67,7 @@ dsm.check<-function(dsm.obj,type=c("deviance","pearson","response"),
 
   ### QQ-plot
   qq.gam(model, rep=rep, level=level, type=type, 
-         rl.col=rl.col, rep.col=rep.col, las=2,...)
+         rl.col=rl.col, rep.col=rep.col, las=1,...)
 
   ### scale-location plot
 #  sl.dat<-data.frame(x=fitted.vals,y=abs(resids))
@@ -94,7 +94,7 @@ dsm.check<-function(dsm.obj,type=c("deviance","pearson","response"),
                       z=resids)
   b<-gam(z~s(x,y,k=gam.k)-1,data=new.dat)
   vis.gam(b,plot.type="contour",main="Fit to residuals",
-          asp=1,view=c("x","y"),type="response",las=2,contour.col="blue") 
+          asp=1,view=c("x","y"),type="response",las=1,contour.col="blue") 
 
 
   ### variogram
@@ -128,7 +128,7 @@ dsm.check<-function(dsm.obj,type=c("deviance","pearson","response"),
   acf.fit <- acf(all.dat,plot=FALSE,type="covariance")
 
   plot(acf.fit$lag[,,2][,1], acf.fit$acf[,,2][,2],type="l", 
-       xlab="Lag",ylab="Correlation", main="Autocorrelogram",las=2)
+       xlab="Lag",ylab="Correlation", main="Autocorrelogram",las=1)
 
   #plot(all.dat)
 
