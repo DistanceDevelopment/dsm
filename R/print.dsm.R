8f528502310682b8e8172b8eab5af8460efd4e0d
David Lawrence Miller <dave@ninepointeightone.net>
2012-11-07 21:35:28 -0500

    print, removed summary and check methods

--- R/print.dsm.R
+++ R/print.dsm.R
@@ -1,8 +1,8 @@
 #' Print a description of a density surface model object
-#' 
+#'
 #' This method just gives a short description of the fitted model. Use the
 #' \code{\link{summary.dsm}} method for more information.
-#' 
+#'
 #' @S3method print dsm
 #' @aliases print.dsm
 #' @method print dsm
@@ -19,20 +19,12 @@ print.dsm<-function(x,...){
   ### General information
 
   cat("\nDensity surface model\n")
-  cat("Response : ", x$model.spec$response, "\n")
-  cat("Offset   : ", x$model.spec$offset, "\n")
+  cat("Response : ",as.character(x$formula)[2] , "\n")
 
   cat("\nDetection function : ",ddf.model.description(x$ddf),"\n")
 
-  ### GAM things...
-  if(x$model.spec$model=="GAM"){
-    cat("\nSummary of GAM\n")
-    cat("\nFormula: ",as.character(x$model.spec$formula)[2],"~",
-                      as.character(x$model.spec$formula)[-c(1,2)],"\n")
-  }else if(x$model.spec$model=="GLM"){
-    cat("Print not implemented for GLMs at the moment\n")
-  }
-  
+  cat("\nFormula: ",as.character(x$formula)[2],"~",
+                    as.character(x$formula)[-c(1,2)],"\n")
   cat("\n")
 
   invisible()
