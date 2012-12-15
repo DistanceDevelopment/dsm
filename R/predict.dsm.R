8cf0bbaeb900a3114fd7fc7e0352befb26e0a801
David Lawrence Miller <dave@ninepointeightone.net>
2012-12-15 13:28:25 +0000

    doc change

--- R/predict.dsm.R
+++ R/predict.dsm.R
@@ -2,7 +2,7 @@
 #'
 #' Make predictions outside (or inside) the covered area.
 #'
-#' @param object a fitted \code{\link{dsm}} object as produced by \code{gam()}.
+#' @param object a fitted \code{\link{dsm}} object as produced by \code{dsm()}.
 #' @param newdata spatially referenced covariates e.g.
 #'                sea temperature, depth, distance to shore, etc.
 #'                Note covariates in this dataframe must have names *identical*
