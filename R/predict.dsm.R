ce5ebc1d76edde12c4602043956b467ed8bc1ab7
David Lawrence Miller <github@ninepointeightone.net>
2014-02-04 12:33:28 -0500

    documentation updates

--- R/predict.dsm.R
+++ R/predict.dsm.R
@@ -3,19 +3,15 @@
 #' Make predictions outside (or inside) the covered area.
 #'
 #' @param object a fitted \code{\link{dsm}} object as produced by \code{dsm()}.
-#' @param newdata spatially referenced covariates e.g.
-#'                sea temperature, depth, distance to shore, etc.
-#'                Note covariates in this dataframe must have names *identical*
-#'                to variable names used in fitting the GAM.
-#' @param off.set area of each of the cells in the prediction grid. Ignored if 
-#'                if their is already a column in \code{newdata} called
-#'                \code{off.set}.
-#'                The link function is applied to the offset inside this 
-#'                function.
-#' @param type what scale should the results be on. The default is 
-#'             \code{"response"} and is almost always what you want.
-#' @param \dots any other arguments passed to \code{\link{predict.gam}} or
-#'              \code{\link{predict.glm}}.
+#' @param newdata spatially referenced covariates e.g. altitude, depth,
+#'  distance to shore, etc. Note covariates in this dataframe must have names
+#'  *identical* to variable names used in fitting the DSM.
+#' @param off.set area of each of the cells in the prediction grid. Ignored if
+#'  their is already a column in \code{newdata} called \code{off.set}.
+#' @param type what scale should the results be on. The default is
+#'  \code{"response"}, see \code{\link{predict.gam}} for an explanation of other
+#'  options (usually not necessary).
+#' @param \dots any other arguments passed to \code{\link{predict.gam}}.
 #' @return predicted values on the response scale (density/abundance).
 #'
 #' @S3method predict dsm
