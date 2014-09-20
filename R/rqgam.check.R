8c92d1c156301f5a646a721eed71a31af13c1af2
David Lawrence Miller <github@ninepointeightone.net>
2014-09-20 18:32:28 -0400

    This actually fixes Tweedie/tw/negbin/nb for rqgam.check
    shouldn't make commits late at night...

--- R/rqgam.check.R
+++ R/rqgam.check.R
@@ -24,16 +24,11 @@
 #' hr.model <- ds(mexdolphins$distdata, max(mexdolphins$distdata$distance),
 #'                key = "hr", adjustment = NULL)
 #'
-#' # fit a simple smooth of x and y
-#' mod1<-dsm(N~s(x,y), hr.model, mexdolphins$segdata, mexdolphins$obsdata)
+#' # fit a simple smooth of x and y with a Tweedie response with estimated
+#' #  p parameter
+#' mod1<-dsm(N~s(x,y), hr.model, mexdolphins$segdata, mexdolphins$obsdata, family=tw())
 #' rqgam.check(mod1)
 rqgam.check<-function(gam.obj,...){
-
-  # for negbin need to set $theta
-  if(grepl("Negative Binomial",gam.obj$family$family)){
-    gam.obj$theta <- gam.obj$family$getTheta()
-  }
-
 
   # layout stuff
   opar <- par(mfrow=c(2,2))
@@ -42,12 +37,27 @@ rqgam.check<-function(gam.obj,...){
   # requires statmod package
 
   # need to do the right thing for mgcv's Tweedie
-  if(grepl("^Tweedie",mod1$family$family)){
+  if(grepl("^Tweedie",gam.obj$family$family)){
+    if(is.null(environment(gam.obj$family$variance)$p)){
+      p.val <- gam.obj$family$getTheta(TRUE)
+      environment(gam.obj$family$variance)$p <- p.val
+    }
     qres <- qres.tweedie(gam.obj)
+  # and for negbin
+  }else if(grepl("^Negative Binomial",gam.obj$family$family)){
+    # need to set $theta
+    if("extended.family" %in% class(gam.obj$family)){
+      # for SNW's extended family, need to set TRUE in getTheta as theta
+      # is on the wrong scale
+      gam.obj$theta <- gam.obj$family$getTheta(TRUE)
+    }else{
+      gam.obj$theta <- gam.obj$family$getTheta()
+    }
+    qres <- qres.nbinom(gam.obj)
   }else{
+    # for everything else
     qres <- qresid(gam.obj)
   }
-
 
   # values of the linear predictor
   linpred <- napredict(gam.obj$na.action, gam.obj$linear.predictors)
@@ -60,7 +70,7 @@ rqgam.check<-function(gam.obj,...){
          xlab="linear predictor",ylab="Randomised quantile residuals",...)
 
   ## histogram
-  hist(qresid(gam.obj), main="Histogram of residuals",
+  hist(qres, main="Histogram of residuals",
        xlab="Randomised quantile residuals",...)
 
 
